58b06070df5cb5b4edf6c61c93f626e4bb1d9d6a
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-08-15 00:53:47 +0100

    Implemented the extract_name function with split() instead of find()

--- listing.py
+++ listing.py
@@ -5,8 +5,9 @@ import os
 
 
 def extract_place(filename):
-	return  filename[filename.find("_") + 1 : filename.find("_", filename.find("_") + 1)]
-
+#version1	return  filename[filename.find("_") + 1 : filename.find("_", filename.find("_") + 1)]
+#version2
+	return filename.split('_')[1]
 
 
 os.chdir("photos")
