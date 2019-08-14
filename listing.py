d8eea82bed872f7980a9019ff9c8b7779a886498
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-08-15 00:42:19 +0100

    Extract name function added

--- listing.py
+++ listing.py
@@ -1,5 +1,15 @@
 import os
 
+
+
+
+
+def extract_place(filename):
+	return  filename[filename.find("_") + 1 : filename.find("_", filename.find("_") + 1)]
+
+
+
 os.chdir("photos")
 originals = os.listdir()
-print(originals)
+print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
+
