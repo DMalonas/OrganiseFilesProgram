5a0f618bdea1108029bd6ecc01273c1f286c3d79
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-08-15 02:27:06 +0100

    Added code to move files to corresponding directories

--- listing.py
+++ listing.py
@@ -11,14 +11,20 @@ def extract_place(filename):
 #version2
 	return filename.split("_")[1]
 
+def organize_photos(directory):
+	os.chdir(directory)
+	places = []
+	originals = os.listdir()
+	print(originals)
+	#print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
+	for file in originals:
+		place = extract_place(file)
+		if place not in places:
+			places.append(place)
+	print(places)
+	make_place_directories(places)
+	for filename in originals:
+		os.rename(filename, os.path.join(extract_place(filename), filename))
 
-os.chdir("photos")
-places = []
-originals = os.listdir()
-#print(extract_place("2016-11-04_Berlin_09/42/22.jpg"))
-for file in originals:
-	place = extract_place(file)
-	if place not in places:
-		places.append(place)
-print(places)
-make_place_directories(places)
+
+organize_photos("photos")
